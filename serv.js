bdbf2fd03bcfa5ffc7fca9c2b41d728e5523ca82
Kas <sanjaykrish2023@vitstudent.ac.in>
2026-01-08 16:48:47 +0530

    event changed

--- serv.js
+++ serv.js
@@ -1,4 +1,4 @@
-document.addEventListener("dblclick",function(e){
+document.addEventListener("click",function(e){
     if(e.target.tagName == "LI"){
         e.target.style.textDecoration = "line-through"
     }
